977fc07b69df407e85b9094921202178f025984c
Tihon49 <tihon49@gmail.com>
2020-06-25 18:03:33 +0300

    tags output added

--- courseDJ/courseDJ/urls.py
+++ courseDJ/courseDJ/urls.py
@@ -24,8 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('<slug:category>/<slug:slug>/', views.PostDetailView.as_view(), name='detail_post'),
     path('<slug:category_name>', views.CategoryView.as_view(), name='category'),
-    path('', views.HomeView.as_view(), name='home'),
-
+    path('', views.HomeView.as_view(), name='home')
 ]
 
 
