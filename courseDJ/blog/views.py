512a293330c44bc2a250102c2ed08dd4eb9e4ff0
Tihon49 <tihon49@gmail.com>
2020-06-29 12:43:06 +0300

    query_set() optimisation

--- courseDJ/blog/views.py
+++ courseDJ/blog/views.py
@@ -19,19 +19,24 @@ class PostDetailView(View):
 
 
 class CategoryView(View):
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=datetime.now(), published=True)
+
     '''Вывод статей'''
     def get(self, request, category_slug=None, tag_slug=None):
         category_list = Category.objects.all()
-        posts = Post.objects.filter(published_date__lte=datetime.now(), published=True)
         # по категории
         if category_slug is not None:
-            posts = Post.objects.filter(category__slug=category_slug,
+            posts = self.get_queryset().filter(category__slug=category_slug,
                                         category__published=True,
                                         published=True)
         # по тегу
         elif tag_slug is not None:
-            posts = Post.objects.filter(tags__slug=tag_slug,
+            posts = self.get_queryset().filter(tags__slug=tag_slug,
                                         published=True)
+        else:
+            posts = self.get_queryset()
+
         if posts.exists():
             template = posts.first().get_category_template()
         else:
